ec7b44fe2ef3453ad248dbd2a9698d73d8227022
ffXder <fjfajardo0624@gmail.com>
2025-04-02 22:49:37 +0800

    changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const audio = new Audio("assets/tumibok-ang-poso-ko.mp3");
 
 //yes button
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Aaaaa, tumibok ang poso ko i like u too <3 ";
+    question.innerHTML = "Aaaaa, sabi na e i like u too <3 ";
     gif.style.width = "380px";
     gif.src = "assets/kisses.gif";
     audio.play();
